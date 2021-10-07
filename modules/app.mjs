6723a51f6dec70d084a810772788c1bf19608eac
dh10110 <dh10110@gmail.com>
2021-10-07 14:28:28 -0400

    Retrieve 2 at once

--- modules/app.mjs
+++ modules/app.mjs
@@ -4,12 +4,21 @@ import * as data from './data.mjs';
 function start() {
     $('#status').text('Fetching Data');
 
-    //data.getDistricts().then(ridings => {
-        //const text = JSON.stringify(ridings, null, 4);
+    data.getRidings().then(data => {
+        const text = JSON.stringify(data, null, 4);
+        $('<article/>')
+            .append($('<h2/>').text('getVotingResults'))
+            .append($('<pre/>').text(text))
+            .appendTo($('#data-container'));
+        $('#status').text('Loaded 1');
+    });
     data.getVotingResults().then(data => {
         const text = JSON.stringify(Array.from(data.values()), null, 4);
-        $('#data').text(text);
-        $('#status').text('Ready');
+        $('<article/>')
+            .append($('<h2/>').text('getVotingResults'))
+            .append($('<pre/>').text(text))
+            .appendTo($('#data-container'));
+        $('#status').text('Loaded 2');
     });
 }
 
