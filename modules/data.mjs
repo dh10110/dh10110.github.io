cb327ab5d7897671534caabf7843d36e86d30f14
dh10110 <dh10110@gmail.com>
2021-10-07 08:42:14 -0400

    fetch text

--- modules/data.mjs
+++ modules/data.mjs
@@ -3,7 +3,7 @@ export async function getDistricts() {
     //Get text file content
     const text = await fetch('/datafiles/districts.txt')
         .catch(err => console.log(err))
-        .then(response => response.body);
+        .then(response => response.text());
     //TODO: process CSV
     return text;
 }
