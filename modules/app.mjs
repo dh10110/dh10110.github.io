01ff0be8b2d76da7b2ffb8e929a3c555223df9e2
dh10110 <dh10110@gmail.com>
2021-10-07 14:49:50 -0400

    refactor

--- modules/app.mjs
+++ modules/app.mjs
@@ -1,23 +1,23 @@
 import $ from './lib/jquery.mjs';
 import * as data from './data.mjs';
 
+function showData(heading, data) {
+    const text = JSON.stringify(data, null, 4);
+    $('<article/>')
+        .append($('<h2/>').text(heading))
+        .append($('<pre/>').text(text))
+        .appendTo($('#data-container'));
+}
+
 function start() {
     $('#status').text('Fetching Data');
 
     data.getRidings().then(data => {
-        const text = JSON.stringify(data, null, 4);
-        $('<article/>')
-            .append($('<h2/>').text('getRidings'))
-            .append($('<pre/>').text(text))
-            .appendTo($('#data-container'));
+        showData('getRidings', data);
         $('#status').text('Loaded 1');
     });
     data.getVotingResults().then(data => {
-        const text = JSON.stringify(Array.from(data.values()), null, 4);
-        $('<article/>')
-            .append($('<h2/>').text('getVotingResults'))
-            .append($('<pre/>').text(text))
-            .appendTo($('#data-container'));
+        showData('getVotingResults', Array.from(data.values()));
         $('#status').text('Loaded 2');
     });
 }
