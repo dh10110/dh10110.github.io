92de8f5666b09770175034f0fda5e747d9c0e11c
dh10110 <dh10110@gmail.com>
2021-10-10 05:40:38 -0400

    Initial refactor html template

--- modules/app.mjs
+++ modules/app.mjs
@@ -39,43 +39,47 @@ async function showResults() {
 
     for (const newRiding of ridings) {
         newRiding.voting = [];
+        newRiding.summary = { total: 0, rejected: 0 };
+        const mapByParty = new Map();
         for (const district_number of newRiding.districts) {
             const districtVoting = voting.get(district_number);
             newRiding.voting.push(districtVoting);
             for (const candidate of districtVoting.candidates) {
-                let partyObj = parties[candidate.party] || parties.default;
-                candidate.color = partyObj.color;
-            }
-            districtVoting.candidates.sort(compareCandidates);
-        }
-        newRiding.summary = newRiding.voting.reduce((sum, dv) => {
-            for (const candidate of dv.candidates) {
                 const partyObj = parties[candidate.party] || parties.default;
-                const key = partyObj.abbr === 'Other' ? 'Other' : candidate.party;
-                const partyTotal = getOrAdd(sum.byParty, key, () => ({
-                    party: key,
-                    color: candidate.color,
+                candidate.color = partyObj.color;
+                const sumKey = partyObj.abbr === 'Other' ? 'Other' : candidate.party;
+                const partyTotal = getOrAdd(mapByParty, sumKey, () => ({
+                    party: sumKey,
+                    color: partyObj.color,
                     votes: 0
                 }));
                 partyTotal.votes += candidate.votes;
             }
-            sum.total += dv.district_total_votes;
-            sum.rejected += dv.district_rejected_ballots;
-            return sum;
-        }, {
-            total: 0,
-            rejected: 0,
-            byParty: new Map()
-        });
+            summary.total += districtVoting.district_total_votes;
+            summary.rejected += districtVoting.district_rejected_ballots;
 
-        const ridingHtml = `
+            districtVoting.candidates.sort(compareCandidates);
+        }
+        newRiding.summary.byParty = Array.from(mapByParty.values());
+        newRiding.summary.byParty.sort(compareCandidates);
+
+        const ridingHtml = makeRidingHtml(newRiding);
+        const container = document.getElementById('vote-initial');
+        container.insertAdjacentHTML('beforeend', ridingHtml);
+    }
+
+    showData('processed ridings', ridings);
+}
+
+function makeRidingHtml(newRiding) {
+    return `
 <article>
     <details class="new-riding">
         <summary>${newRiding.riding} ${newRiding.voting.map(dv => 
             `<span style="color: ${dv.candidates[0].color};">⬤</span>`
         ).join('')}
             <div style="display:flex;">
-            ${Array.from(newRiding.summary.byParty.values()).map(pt =>
+            ${newRiding.summary.byParty.map(pt =>
                 `<div style="height: 10px; background-color: ${pt.color}; flex-grow: ${(pt.votes / newRiding.summary.total).toFixed(3)}"></div>`
             ).join('')}
                 <div style="height: 10px; background-color: #aaa; flex-grow: ${(newRiding.summary.rejected / newRiding.summary.total).toFixed(3)}"></div>
@@ -85,44 +89,56 @@ async function showResults() {
             <details class="old-district">
                 <summary>Totals</summary>
                 <section class="details-body">
-                ${Array.from(newRiding.summary.byParty.values()).map(pt => `
-                    <div class="vote-total">
-                        <div><span style="color: ${pt.color};">⬤</span>${pt.party}</div>
-                        ${makeMeter(pt.votes, newRiding.summary.total, pt.color)}
-                    </div>
-                `).join('')}
-                    <div class="vote-total">
-                        <div>Rejected Ballots</div>
-                        ${makeMeter(newRiding.summary.rejected, newRiding.summary.total, '#aaa')}
-                    </div>
+                    ${newRiding.summary.byParty.map(pt => 
+                        makeVoteLine({
+                            heading: pt.party,
+                            votes: pt.votes,
+                            voteTotal: newRiding.summary.total,
+                            color: pt.color 
+                        }).join('')
+                    )}
+                    ${makeVoteLine({
+                        heading: 'Rejected Ballots',
+                        votes: newRiding.summary.rejected,
+                        voteTotal: newRiding.summary.total,
+                        color: '#aaa'
+                    })}
                 </section>
             </details>
             ${newRiding.voting.map(dv => `
             <details class="old-district">
                 <summary>${dv.district_name} <span style="color: ${dv.candidates[0].color};">⬤</span></summary>
                 <section class="details-body">
-                ${dv.candidates.map(c => `
-                    <div class="vote-total">
-                        <div><span style="color: ${c.color};">⬤</span>${c.surname} - ${c.party}</div>
-                        ${makeMeter(c.votes, dv.district_total_votes, c.color)}
-                    </div>
-                `).join('')}
-                    <div class="vote-total">
-                        <div>Rejected Ballots</div>
-                        ${makeMeter(dv.district_rejected_ballots, dv.district_total_votes, '#aaa')}
-                    </div>
+                    ${dv.candidates.map(c => 
+                        makeVoteLine({
+                            heading: `${c.surname} - ${c.party}`,
+                            votes: c.votes,
+                            voteTotal: dv.district_total_votes,
+                            color: c.color 
+                        }).join('')
+                    )}
+                    ${makeVoteLine({
+                        heading: 'Rejected Ballots',
+                        votes: dv.district_rejected_ballots,
+                        voteTotal: dv.district_total_votes,
+                        color: '#aaa'
+                    })}
                 </section>
             </details>
         `).join('')}
         </section>
     </details>
 </article>
-        `;
-        const container = document.getElementById('vote-initial');
-        container.insertAdjacentHTML('beforeend', ridingHtml);
-    }
+    `;
+}
 
-    showData('processed ridings', ridings);
+function makeVoteLine({ heading, votes, voteTotal, color = '#aaa'} = {}) {
+    return `
+                    <div class="vote-total">
+                        <div><span style="color: ${color};">⬤</span>${heading}</div>
+                        ${makeMeter(votes, voteTotal, color)}
+                    </div>
+    `;
 }
 
 function makeMeter(numerator, denominator, barColor) {
