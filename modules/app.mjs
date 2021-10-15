02d43cd18a110ec43f595542600b0ffb3b668598
dh10110 <dh10110@gmail.com>
2021-10-15 19:27:46 -0400

    progress tweak

--- modules/app.mjs
+++ modules/app.mjs
@@ -116,7 +116,11 @@ function runElectionWorker(stvDistrict) {
 
     const showProgress = function () {
         if (progress !== lastProgress) {
-            document.getElementById(statusId).textContent = `(${progress})`;
+            if (!progress) {
+                document.getElementById(statusId).textContent = '';
+            }else {
+                document.getElementById(statusId).textContent = `(${progress})`;
+            }
             lastProgress = progress;
         }
         if (progress) {
